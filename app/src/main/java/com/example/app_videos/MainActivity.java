1e65734963a792025f2a4049ad06f3aa53fcf57d
AjMaster <52114647+Jesus017x@users.noreply.github.com>
2020-08-08 17:20:28 -0400

    Update 1 - Register and Login Update 2 - Register, Login and MainActivity separation Update 2 - Colorcito morado xD Update 3 - Notifications (Fcm.class), gonna keep working on it Update 3.1 - Should be working again Update 4 - Push notifications (via cloud messaging) working with Register button. Update 5 - Updating main Update 5.1 - Full push Update 5.2 - Choose video added and fixed the main layout

--- app/src/main/java/com/example/app_videos/MainActivity.java
+++ app/src/main/java/com/example/app_videos/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -28,19 +29,18 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private VideoView mainVideoView;
-    private ImageView playBtn;
-    private TextView currentTimer;
-    private TextView durationTimer;
-    private ProgressBar progressBar;
-    private ProgressBar progressBarBuffering;
-
-    private boolean isPlaying;
-
+    VideoView mainVideoView;
+    Button uploadBtn;
+    ProgressBar mainProgressBar;
+    EditText videoName;
+    MediaController mediaController;
     private Uri videoUri;
+    private ImageView playBtn;
+    private ProgressBar progressBar;
 
-    private int current = 0;
-    private int duration = 0;
+    //private boolean isPlaying;
+
+    private static final int PICK_VIDEO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,50 +49,37 @@ public class MainActivity extends AppCompatActivity {
 
         //Reproductor de video
 
-        isPlaying = false;
+        //isPlaying = false;
 
         mainVideoView = (VideoView) findViewById(R.id.mainVideoView);
+        uploadBtn = (Button) findViewById(R.id.btn_upload);
+        mainProgressBar = (ProgressBar) findViewById(R.id.progressBar_main);
+        videoName = (EditText) findViewById(R.id.video_name);
         playBtn = (ImageView) findViewById(R.id.playBtn);
-        currentTimer = (TextView) findViewById(R.id.currentTimer);
-        durationTimer = (TextView) findViewById(R.id.durationTimer);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        progressBar.setMax(100);
-        progressBarBuffering = (ProgressBar) findViewById(R.id.progressBarBuffering);
+        mediaController = new MediaController(this);
+        mainVideoView.setMediaController(mediaController);
+        mainVideoView.start();
 
-        videoUri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/appvideos-b3b88.appspot.com/o/Why%20Programming%20Is%20Important.mp4?alt=media&token=c588f403-f562-4c52-a064-b2e221d8ebff");
+        //videoUri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/appvideos-b3b88.appspot.com/o/Why%20Programming%20Is%20Important.mp4?alt=media&token=c588f403-f562-4c52-a064-b2e221d8ebff");
 
-        mainVideoView.setVideoURI(videoUri);
-        mainVideoView.requestFocus();
+        /*mainVideoView.setVideoURI(videoUri);
+        mainVideoView.requestFocus();*/
 
-        mainVideoView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
-            @Override
-            public boolean onInfo(MediaPlayer mp, int i, int il) {
 
-                if(i == mp.MEDIA_INFO_BUFFERING_START){
-                    progressBarBuffering.setVisibility(View.VISIBLE);
-
-                }
-                else if(i == mp.MEDIA_INFO_BUFFERING_END){
-                    progressBarBuffering.setVisibility(View.INVISIBLE);
-                }
-                return false;
-            }
-        });
-
-        mainVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
+        /*mainVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
             @Override
             public void onPrepared(MediaPlayer mp){
                 duration = mp.getDuration()/1000;
                 String durationString = String.format("%02d:%02d", duration/60, duration %60);
                 durationTimer.setText(durationString);
             }
-        });
+        });*/
 
-        mainVideoView.start();
-        isPlaying = true;
-        playBtn.setImageResource(R.drawable.pausebtn);
+        //mainVideoView.start();
+        //isPlaying = true;
+        //playBtn.setImageResource(R.drawable.pausebtn);
 
-        new VideoProgress().execute();
+        /*new VideoProgress().execute();
 
         playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -107,23 +94,27 @@ public class MainActivity extends AppCompatActivity {
                     playBtn.setImageResource(R.drawable.pausebtn);
                 }
             }
-        });
+        });*/
 
     }
-
+    /*
     @Override
     protected void onStop() {
         super.onStop();
 
         isPlaying = false;
-    }
+    }*/
 
     public void ChooseVideo(View view) {
+        Intent intent = new Intent();
+        intent.setType("video/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_VIDEO);
     }
 
     public void ShowVideos(View view) {
     }
-
+    /*
     public class VideoProgress extends AsyncTask<Void, Integer, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
@@ -154,5 +145,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
-    }
+    }*/
 }
