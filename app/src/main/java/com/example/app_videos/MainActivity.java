ec2adf2260bd2b32a16b2d43bf35181f12126c2c
AjMaster <52114647+Jesus017x@users.noreply.github.com>
2020-08-08 22:20:32 -0400

    Update 1 - Register and Login Update 2 - Register, Login and MainActivity separation Update 2 - Colorcito morado xD Update 3 - Notifications (Fcm.class), gonna keep working on it Update 3.1 - Should be working again Update 4 - Push notifications (via cloud messaging) working with Register button. Update 5 - Updating main Update 5.1 - Full push Update 5.2 - Choose video added and fixed the main layout Update 6 - Final push ("Are ya winning son?")

--- app/src/main/java/com/example/app_videos/MainActivity.java
+++ app/src/main/java/com/example/app_videos/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.app_videos;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -11,7 +15,10 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -22,64 +29,91 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    VideoView mainVideoView;
-    Button uploadBtn;
-    ProgressBar mainProgressBar;
-    EditText videoName;
-    MediaController mediaController;
+    private VideoView mainVideoView, mainVideoView2;
+    private ImageView playBtn, playBtn2;
+    private TextView currentTimer, currentTimer2;
+    private TextView durationTimer, durationTimer2;
+    private ProgressBar progressBar, progressBar2;
+
+    private boolean isPlaying;
+
     private Uri videoUri;
-    private ImageView playBtn;
-    private ProgressBar progressBar;
+    private Uri videoUri2;
 
-    //private boolean isPlaying;
-
-    private static final int PICK_VIDEO = 1;
+    private int current = 0;
+    private int current2 = 0;
+    private int duration = 0;
+    private int duration2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Reproductor de video
-
-        //isPlaying = false;
+        isPlaying = false;
 
         mainVideoView = (VideoView) findViewById(R.id.mainVideoView);
-        uploadBtn = (Button) findViewById(R.id.btn_upload);
-        mainProgressBar = (ProgressBar) findViewById(R.id.progressBar_main);
-        videoName = (EditText) findViewById(R.id.video_name);
+        mainVideoView2 = (VideoView) findViewById(R.id.mainVideoView2);
         playBtn = (ImageView) findViewById(R.id.playBtn);
-        mediaController = new MediaController(this);
-        mainVideoView.setMediaController(mediaController);
-        mainVideoView.start();
+        playBtn2 = (ImageView) findViewById(R.id.playBtn2);
+        currentTimer = (TextView) findViewById(R.id.currentTimer);
+        currentTimer2 = (TextView) findViewById(R.id.currentTimer2);
+        durationTimer = (TextView) findViewById(R.id.durationTimer);
+        durationTimer2 = (TextView) findViewById(R.id.durationTimer2);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar2 = (ProgressBar) findViewById(R.id.progressBar2);
+        progressBar.setMax(100);
+        progressBar2.setMax(100);
 
-        //videoUri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/appvideos-b3b88.appspot.com/o/Why%20Programming%20Is%20Important.mp4?alt=media&token=c588f403-f562-4c52-a064-b2e221d8ebff");
+        videoUri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/appvideos-b3b88.appspot.com/o/Why%20Programming%20Is%20Important.mp4?alt=media&token=c588f403-f562-4c52-a064-b2e221d8ebff");
+        videoUri2 = Uri.parse("https://firebasestorage.googleapis.com/v0/b/appvideos-b3b88.appspot.com/o/Introduction%20to%20Cybersecurity.mp4?alt=media&token=f7100b05-34a3-40ff-8f47-fc649d4fcf0d");
 
-        /*mainVideoView.setVideoURI(videoUri);
-        mainVideoView.requestFocus();*/
+        mainVideoView.setVideoURI(videoUri);
+        mainVideoView2.setVideoURI(videoUri2);
+        mainVideoView.requestFocus();
+        mainVideoView2.requestFocus();
 
 
-        /*mainVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
+        mainVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
             @Override
             public void onPrepared(MediaPlayer mp){
                 duration = mp.getDuration()/1000;
                 String durationString = String.format("%02d:%02d", duration/60, duration %60);
                 durationTimer.setText(durationString);
             }
-        });*/
+        });
+        mainVideoView2.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
+            @Override
+            public void onPrepared(MediaPlayer mp){
+                duration = mp.getDuration()/1000;
+                String durationString = String.format("%02d:%02d", duration/60, duration %60);
+                durationTimer2.setText(durationString);
+            }
+        });
 
-        //mainVideoView.start();
-        //isPlaying = true;
-        //playBtn.setImageResource(R.drawable.pausebtn);
 
-        /*new VideoProgress().execute();
+        mainVideoView.start();
+        mainVideoView2.start();
+        isPlaying = true;
+        playBtn.setImageResource(R.drawable.pausebtn);
+        playBtn2.setImageResource(R.drawable.pausebtn);
+
+        new VideoProgress().execute();
 
         playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,27 +128,30 @@ public class MainActivity extends AppCompatActivity {
                     playBtn.setImageResource(R.drawable.pausebtn);
                 }
             }
-        });*/
+        });
+        playBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPlaying){
+                    mainVideoView2.pause();
+                    isPlaying = false;
+                    playBtn2.setImageResource(R.drawable.playbtn);
+                } else {
+                    mainVideoView2.start();
+                    isPlaying = true;
+                    playBtn2.setImageResource(R.drawable.pausebtn);
+                }
+            }
+        });
 
     }
-    /*
+
     @Override
     protected void onStop() {
         super.onStop();
-
         isPlaying = false;
-    }*/
-
-    public void ChooseVideo(View view) {
-        Intent intent = new Intent();
-        intent.setType("video/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, PICK_VIDEO);
     }
 
-    public void ShowVideos(View view) {
-    }
-    /*
     public class VideoProgress extends AsyncTask<Void, Integer, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
@@ -122,10 +159,12 @@ public class MainActivity extends AppCompatActivity {
 
                 if(isPlaying){
                     current = mainVideoView.getCurrentPosition() / 1000;
+                    current2 = mainVideoView2.getCurrentPosition() / 1000;
                     publishProgress(current);
+                    publishProgress(current2);
                 }
 
-            } while (progressBar.getProgress() <= 100);
+            } while (progressBar.getProgress() <= 100 || progressBar2.getProgress() <= 100);
 
             return null;
         }
@@ -136,14 +175,16 @@ public class MainActivity extends AppCompatActivity {
             try{
                 int currentPercent = values[0] * 100/duration;
                 progressBar.setProgress(currentPercent);
+                progressBar2.setProgress(currentPercent);
 
                 String currentString = String.format("%02d:%02d", values[0] / 60, values[0] % 60);
 
                 currentTimer.setText(currentString);
+                currentTimer2.setText(currentString);
 
             } catch(Exception e) {
 
             }
         }
-    }*/
+    }
 }
